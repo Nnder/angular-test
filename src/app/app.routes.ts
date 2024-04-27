943988cdc9fdb271ae27aee4d8a3db37fcc40904
nnder <asfahaikrkt6k56@yandex.ru>
2024-04-28 01:36:16 +0500

    fontawesome changer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { IconComponent } from './icon/icon.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: IconComponent },
+];
---
+++ src/app/icon/icon.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { faArrowCircleRight, faArrowDown, faArrowUp, faCoffee} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAdn, faAngular, faReact, faVuejs } from '@fortawesome/free-brands-svg-icons';
+
+@Component({
+  selector: 'app-icon',
+  templateUrl: './icon.component.html',
+  standalone: true,
+  imports: [FontAwesomeModule],
+})
+export class IconComponent {
+    icons = [faArrowDown, faCoffee, faArrowUp, faAdn, faAngular, faReact, faVuejs, faArrowCircleRight];
+    icon = this.icons[0];
+
+  updateIcon() {
+    setTimeout(()=>{
+        const random = this.getRandomIcon()
+        console.log(random)
+        this.icon = this.icons[random]
+    }, 3000)
+    
+  }
+  getRandomIcon() {
+    return Math.round(Math.random() * (this.icons.length-1 - 0) + 0);
+ }
+}
